5d938f6ac567531e174e97265388a3cab32024c7
PrisT-11 <priscillathung11@gmail.com>
2026-08-08 19:28:57 +0800

    Image clickable

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -1,14 +1,36 @@
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Backdrop from '@mui/material/Backdrop';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import React from 'react';
 
 const noPadding = {
     padding: "0px",
     '@media (min-width: 600px)': {
-          padding: "0px"
-        }
+        padding: "0px"
+    }
 };
 
-export default function PortfolioItem({ title, year, image  }) {
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+export default function PortfolioItem({ title, year, image }) {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
         <Container sx={noPadding}>
             <Box
@@ -19,19 +41,49 @@ export default function PortfolioItem({ title, year, image  }) {
                     overflow: 'hidden',
                 }}
             >
-                <img
-                    src={image}
-                    alt={title}
-                    style={{
-                        position: 'absolute',
-                        top: 0,
-                        left: 0,
-                        width: '100%',
-                        height: '100%',
-                        objectFit: 'cover',
-                    }}
-                />
+                <Button 
+                disableRipple  
+                sx={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
+                onClick={handleOpen}>
+                    <img
+                        src={image}
+                        alt={title}
+                        style={{
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            width: '100%',
+                            height: '100%',
+                            objectFit: 'cover',
+                        }}
+                    />
+                </Button>
             </Box>
+
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                slots={{ backdrop: Backdrop }}
+                slotProps={{
+                    backdrop: {
+                        timeout: 500,
+                    },
+                }}
+            >
+                <Fade in={open}>
+                    <Box sx={style}>
+                        <Typography id="transition-modal-title" variant="h6" component="h2">
+                            Text in a modal
+                        </Typography>
+                        <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                        </Typography>
+                    </Box>
+                </Fade>
+            </Modal>
         </Container>
     );
 }
--- src/components/PortfolioList.jsx
+++ src/components/PortfolioList.jsx
@@ -3,33 +3,11 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
-import Backdrop from '@mui/material/Backdrop';
-import Fade from '@mui/material/Fade';
-import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import PortfolioItem from './PortfolioItem';
-import UnityLogo from "../assets/unity-logo.svg";
 
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
 
 export default function PortfolioList() {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
   return (
     <div>
     <Container maxWidth="xl">
@@ -53,39 +31,11 @@ export default function PortfolioList() {
                 alt={item.title}
                 loading="lazy"
               />
-              <Button variant="contained" color="primary" onClick={handleOpen} sx={{ marginTop: "10px", width: "100%" }}>
-                Open Modal
-              </Button>
             </ImageListItem>
           ))}
         </ImageList>
       </Box>
     </Container>
-
-    <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        slots={{ backdrop: Backdrop }}
-        slotProps={{
-          backdrop: {
-            timeout: 500,
-          },
-        }}
-      >
-        <Fade in={open}>
-          <Box sx={style}>
-            <Typography id="transition-modal-title" variant="h6" component="h2">
-              Text in a modal
-            </Typography>
-            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography>
-          </Box>
-        </Fade>
-      </Modal>
     </div>
   );
 }
